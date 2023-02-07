a630540847b90a8dbfe6971a1f11771e99e44c5b
voyager <cdspacenoob@gmail.com>
2023-02-07 16:59:20 +0900

    apply custom hook & refactoring

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -1,18 +1,17 @@
-import { useState } from "react";
+import { useState, useCallback } from 'react';
 
-const useHttp = (requestConfig, applyData) => {
+const useHttp = () => {
     const [isLoading, setIsLoading] = useState(false);
     const [error, setError] = useState(null);
 
-    // fetchTasks -> sendRequest로 변경. 좀 더 범용적인 느낌의 이름으로 변경했다. 
-    const sendRequest = async (taskText) => {
+    const sendRequest = useCallback(async (requestConfig, applyData) => {
         setIsLoading(true);
         setError(null);
         try {
             const response = await fetch(requestConfig.url, {
-                method: requestConfig.method,
-                headers: requestConfig.headers,
-                body: JSON.stringify(requestConfig.body),
+                method: requestConfig.method ? requestConfig.method : 'GET',
+                headers: requestConfig.headers ? requestConfig.headers : {},
+                body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
             });
 
             if (!response.ok) {
@@ -21,29 +20,17 @@ const useHttp = (requestConfig, applyData) => {
 
             const data = await response.json();
             applyData(data);
-
-            // const loadedTasks = [];
-
-            // for (const taskKey in data) {
-            //     loadedTasks.push({ id: taskKey, text: data[taskKey].text });
-            // }
-
-            // setTasks(loadedTasks);
         } catch (err) {
             setError(err.message || 'Something went wrong!');
         }
         setIsLoading(false);
-    };
+    }, []);
 
-    return ({
+    return {
         isLoading,
-        // isLoading: isLoading,        
-        // key, value가 동일할 때는 축약해서 적을 수 있다. 알아서 인식한다. 
         error,
-        // error: error,
         sendRequest,
-        // sendRequest: sendRequest,
-    });
+    };
 };
 
 export default useHttp;
